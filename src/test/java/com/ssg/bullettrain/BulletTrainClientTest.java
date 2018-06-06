7e9fddcb2dc5dd716845dd2399e5af49110e3898
Pavlo Maks <pavlo@solidstategroup.com>
2018-06-06 12:15:44 +0100

    unit tests fixes

--- src/test/java/com/ssg/bullettrain/BulletTrainClientTest.java
+++ src/test/java/com/ssg/bullettrain/BulletTrainClientTest.java
@@ -12,7 +12,7 @@ import static org.testng.Assert.assertTrue;
  */
 public class BulletTrainClientTest {
 
-    private static final String API_KEY = "[myapi_key]";
+    private static final String API_KEY = "QjgYur4LQTwe5HpvbvhpzK";
 
     @Test(groups = "integration")
     public void testClient_When_Get_Features_Then_Success() {
@@ -21,10 +21,14 @@ public class BulletTrainClientTest {
                 .setApiKey(API_KEY)
                 .build();
 
-        List<FeatureFlag> featureFlags = bulletClient.getFeatureFlags();
+        List<Flag> featureFlags = bulletClient.getFeatureFlags();
 
         assertNotNull(featureFlags, "Should feature flags back");
         assertTrue(bulletClient.getFeatureFlags().size() > 0, "Should have test featureFlags back");
+
+        for(Flag flag: featureFlags){
+            assertNotNull(flag.getFeature(), "Flag should have feature");
+        }
     }
 
     @Test(groups = "integration")
@@ -34,13 +38,18 @@ public class BulletTrainClientTest {
                 .setApiKey(API_KEY)
                 .build();
 
-        User user = new User();
+        // context user
+        FeatureUser user = new FeatureUser();
         user.setIdentifier("bullet_train_sample_user");
 
-        List<FeatureFlag> featureFlags = bulletClient.getFeatureFlags(user);
+        List<Flag> featureFlags = bulletClient.getFeatureFlags(user);
 
         assertNotNull(featureFlags, "Should feature flags back");
         assertTrue(bulletClient.getFeatureFlags().size() > 0, "Should have test featureFlags back");
+
+        for(Flag flag: featureFlags){
+            assertNotNull(flag.getFeature(), "Flag should have feature");
+        }
     }
 
 }
---
+++ src/test/java/com/ssg/bullettrain/FlagTest.java
@@ -0,0 +1,43 @@
+package com.ssg.bullettrain;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+/**
+ *
+ */
+public class FlagTest {
+
+    private static final String json = "{\n" +
+            "    \"id\": 2,\n" +
+            "    \"feature\": {\n" +
+            "      \"id\": 2,\n" +
+            "      \"name\": \"font_size\",\n" +
+            "      \"created_date\": \"2018-06-04T12:51:18.646762Z\",\n" +
+            "      \"initial_value\": \"10\",\n" +
+            "      \"description\": \"test description\",\n" +
+            "      \"project\": 2\n" +
+            "    },\n" +
+            "    \"feature_state_value\": 10,\n" +
+            "    \"enabled\": true,\n" +
+            "    \"environment\": 2,\n" +
+            "    \"identity\": null\n" +
+            "  }";
+
+    @Test(groups = "integration")
+    public void test_When_Parsed_Then_Success() throws IOException {
+
+        Flag flag = new Flag();
+        flag.parse(json);
+
+        assertNotNull(flag.getStateValue(), "Should have flag value");
+        assertTrue(flag.isEnabled(), "Flag should be enabled");
+        assertNotNull(flag.getFeature(), "Flag should have feature");
+        assertNotNull(flag.getFeature().getName(), "Feature should have name");
+        assertNotNull(flag.getFeature().getDescription(), "Feature should have description");
+    }
+}
