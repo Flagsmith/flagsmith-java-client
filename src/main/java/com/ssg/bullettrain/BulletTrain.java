73cf53a9123c02eacac106b6900b3ff5cdf113ca
Pavlo Maks <pavlo@solidstategroup.com>
2018-06-06 13:49:05 +0100

    Initial work for main sdk class

---
+++ src/main/java/com/ssg/bullettrain/BulletTrain.java
@@ -0,0 +1,12 @@
+package com.ssg.bullettrain;
+
+/**
+ * Created by Pavlo Maksymchuk.
+ */
+public class BulletTrain {
+
+    /**
+     * Storage to persist feature within {@link CrudStore}.
+     */
+    private CrudStore store = new InMemoryStore();
+}
