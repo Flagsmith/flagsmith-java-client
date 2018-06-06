4db309ade902a646de023f1d91e7a5f273e49c58
Pavlo Maks <pavlo@solidstategroup.com>
2018-06-06 13:46:53 +0100

    Implemented CRUD store support

---
+++ src/main/java/com/ssg/bullettrain/CrudStore.java
@@ -0,0 +1,17 @@
+package com.ssg.bullettrain;
+
+/**
+ * Interface for a basic store to support persistence for Flags
+ *
+ * Created by Pavlo Maksymchuk.
+ */
+public interface CrudStore<T extends Flag> {
+
+    T create(Flag flag);
+
+    Flag read(String id);
+
+    Flag update(Flag flag);
+
+    void delete(String id);
+}
