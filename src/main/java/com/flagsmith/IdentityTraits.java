32962b73d9671ed4193ace43ab883ae06a784632
Pavlo Maks <pavlo@solidstategroup.com>
2021-04-22 13:25:53 +0100

    code refactor

--- src/main/java/com/flagsmith/IdentityTraits.java
+++ src/main/java/com/flagsmith/IdentityTraits.java
@@ -1,16 +1,14 @@
 package com.flagsmith;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Data;
-
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
+import lombok.Data;
 
 /**
  * DTO for wrapping user identifier and list of user Traits.
@@ -20,30 +18,36 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class IdentityTraits implements Serializable {
 
-    private String identifier;
-    private List<Trait> traits;
+  private String identifier;
+  private List<Trait> traits;
 
-    @JsonIgnore
-    public void parse(String data) throws IOException {
-        ObjectMapper mapper = MapperFactory.getMappper();
-        IdentityTraits prototype = mapper.readValue(data, IdentityTraits.class);
-        fromPrototype(prototype);
-    }
+  /**
+   * Parses given string into IdentityTraits object.
+   *
+   * @param data a json string representing IdentityTraits
+   * @throws IOException when failed to parse data
+   */
+  @JsonIgnore
+  public void parse(String data) throws IOException {
+    ObjectMapper mapper = MapperFactory.getMappper();
+    IdentityTraits prototype = mapper.readValue(data, IdentityTraits.class);
+    fromPrototype(prototype);
+  }
 
-    @JsonIgnore
-    private void fromPrototype(IdentityTraits prototype) {
-        setIdentifier(prototype.getIdentifier());
-        setTraits(prototype.getTraits());
-    }
+  @JsonIgnore
+  private void fromPrototype(IdentityTraits prototype) {
+    setIdentifier(prototype.getIdentifier());
+    setTraits(prototype.getTraits());
+  }
 
-    @JsonIgnore
-    @Override
-    public String toString() {
-        ObjectMapper mapper = MapperFactory.getMappper();
-        try {
-            return mapper.writeValueAsString(this);
-        } catch (JsonProcessingException e) {
-            return super.toString();
-        }
+  @JsonIgnore
+  @Override
+  public String toString() {
+    ObjectMapper mapper = MapperFactory.getMappper();
+    try {
+      return mapper.writeValueAsString(this);
+    } catch (JsonProcessingException e) {
+      return super.toString();
     }
+  }
 }
