dd3fad22b636c8f7332b8420b36fc6f893e762ed
Pavlo Maks <pavlo@solidstategroup.com>
2021-05-04 16:19:32 +0100

    code refactor

--- src/main/java/com/flagsmith/FlagsAndTraits.java
+++ src/main/java/com/flagsmith/FlagsAndTraits.java
@@ -1,16 +1,14 @@
 package com.flagsmith;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Data;
-
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
+import lombok.Data;
 
 /**
  * Holds a list of feature flags and user traits.
@@ -20,30 +18,36 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class FlagsAndTraits implements Serializable {
 
-    private List<Flag> flags;
-    private List<Trait> traits;
+  private List<Flag> flags;
+  private List<Trait> traits;
 
-    @JsonIgnore
-    public void parse(String data) throws IOException {
-        ObjectMapper mapper = MapperFactory.getMappper();
-        FlagsAndTraits prototype = mapper.readValue(data, FlagsAndTraits.class);
-        fromPrototype(prototype);
-    }
+  /**
+   * Parses given string into FlagsAndTraits object.
+   *
+   * @param data a json string representing FlagsAndTraits
+   * @throws IOException when failed to parse data
+   */
+  @JsonIgnore
+  public void parse(String data) throws IOException {
+    ObjectMapper mapper = MapperFactory.getMappper();
+    FlagsAndTraits prototype = mapper.readValue(data, FlagsAndTraits.class);
+    fromPrototype(prototype);
+  }
 
-    @JsonIgnore
-    private void fromPrototype(FlagsAndTraits prototype) {
-        setFlags(prototype.getFlags());
-        setTraits(prototype.getTraits());
-    }
+  @JsonIgnore
+  private void fromPrototype(FlagsAndTraits prototype) {
+    setFlags(prototype.getFlags());
+    setTraits(prototype.getTraits());
+  }
 
-    @JsonIgnore
-    @Override
-    public String toString() {
-        ObjectMapper mapper = MapperFactory.getMappper();
-        try {
-            return mapper.writeValueAsString(this);
-        } catch (JsonProcessingException e) {
-            return super.toString();
-        }
+  @JsonIgnore
+  @Override
+  public String toString() {
+    ObjectMapper mapper = MapperFactory.getMappper();
+    try {
+      return mapper.writeValueAsString(this);
+    } catch (JsonProcessingException e) {
+      return super.toString();
     }
+  }
 }
