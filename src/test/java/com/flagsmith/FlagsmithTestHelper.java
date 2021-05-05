21b32975657fc91271fe73e93d74d11c47b1c8ea
Pavlo Maks <pavlo@solidstategroup.com>
2021-05-05 13:05:27 +0100

    code reformat

--- src/test/java/com/flagsmith/FlagsmithTestHelper.java
+++ src/test/java/com/flagsmith/FlagsmithTestHelper.java
@@ -1,30 +1,32 @@
 package com.flagsmith;
 
+import static com.flagsmith.IntegrationSuiteTest.BACKEND_PORT;
+
 import com.google.common.collect.ImmutableMap;
 import io.restassured.RestAssured;
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
-
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static com.flagsmith.IntegrationSuiteTest.BACKEND_PORT;
-
 public class FlagsmithTestHelper {
 
-  public static ProjectEnvironment createProjectEnvironment(String projectName, String environmentName) {
+  public static ProjectEnvironment createProjectEnvironment(String projectName,
+      String environmentName) {
     return createProjectEnvironment(projectName, environmentName, false);
   }
 
-  public static ProjectEnvironment createProjectEnvironment(String projectName, String environmentName, boolean cached) {
+  public static ProjectEnvironment createProjectEnvironment(String projectName,
+      String environmentName, boolean cached) {
     final int projectId = createProject(projectName, IntegrationSuiteTest.TestData.organisationId);
 
     final Map<String, Object> environment = createEnvironment(environmentName, projectId);
     final String environmentApiKey = (String) environment.get("api_key");
 
     FlagsmithClient.Builder clientBuilder = FlagsmithClient.newBuilder()
-        .withApiUrl(String.format("http://localhost:%d/api/v1/", IntegrationSuiteTest.TestData.backend.getMappedPort(BACKEND_PORT)))
+        .withApiUrl(String.format("http://localhost:%d/api/v1/",
+            IntegrationSuiteTest.TestData.backend.getMappedPort(BACKEND_PORT)))
         .setApiKey(environmentApiKey);
     if (cached) {
       clientBuilder.withCache(FlagsmithCacheConfig.newBuilder()
@@ -132,15 +134,18 @@ public class FlagsmithTestHelper {
             .then()
             .statusCode(200);
       } else {
-        throw new IllegalStateException("Unable to decide which feature-state to update: " + features);
+        throw new IllegalStateException(
+            "Unable to decide which feature-state to update: " + features);
       }
     }
   }
 
-  public static void switchFlagForUser(int featureId, int userIdentityId, boolean enabled, String apiKey) {
+  public static void switchFlagForUser(int featureId, int userIdentityId, boolean enabled,
+      String apiKey) {
     final List<Map<String, Object>> features = RestAssured.given()
         .headers(defaultHeaders())
-        .get("/api/v1/environments/{apiKey}/identities/{identityId}/featurestates/?feature={featureId}",
+        .get(
+            "/api/v1/environments/{apiKey}/identities/{identityId}/featurestates/?feature={featureId}",
             apiKey, userIdentityId, featureId)
         .then()
         .statusCode(200)
@@ -154,7 +159,8 @@ public class FlagsmithTestHelper {
               "feature", featureId
           ))
           .headers(defaultHeaders())
-          .post("/api/v1/environments/{apiKey}/identities/{identityId}/featurestates/", apiKey, userIdentityId)
+          .post("/api/v1/environments/{apiKey}/identities/{identityId}/featurestates/", apiKey,
+              userIdentityId)
           .then()
           .statusCode(201);
     } else {
@@ -175,17 +181,20 @@ public class FlagsmithTestHelper {
                 "feature", featureId
             ))
             .headers(defaultHeaders())
-            .put("/api/v1/environments/{apiKey}/identities/{identityId}/featurestates/{featureStateId}/",
+            .put(
+                "/api/v1/environments/{apiKey}/identities/{identityId}/featurestates/{featureStateId}/",
                 apiKey, userIdentityId, featureStateId)
             .then()
             .statusCode(200);
       } else {
-        throw new IllegalStateException("Unable to decide which feature-state to update: " + features);
+        throw new IllegalStateException(
+            "Unable to decide which feature-state to update: " + features);
       }
     }
   }
 
-  public static void assignTraitToUserIdentity(String userIdentifier, String traitKey, Object traitValue, String apiKey) {
+  public static void assignTraitToUserIdentity(String userIdentifier, String traitKey,
+      Object traitValue, String apiKey) {
     RestAssured.given()
         .body(ImmutableMap.of(
             "identity", ImmutableMap.of("identifier", userIdentifier),
@@ -255,7 +264,14 @@ public class FlagsmithTestHelper {
     return user;
   }
 
+  public static Headers defaultHeaders() {
+    return new Headers(
+        new Header("Content-type", "application/json"),
+        new Header("Authorization", "Token " + IntegrationSuiteTest.TestData.token));
+  }
+
   public static abstract class Feature {
+
     final String type;
     final String name;
     final String description;
@@ -270,6 +286,7 @@ public class FlagsmithTestHelper {
   }
 
   public static class FlagFeature extends Feature {
+
     final boolean enabled;
 
     protected FlagFeature(String name, String description, int projectId, boolean enabled) {
@@ -279,6 +296,7 @@ public class FlagsmithTestHelper {
   }
 
   public static class ConfigFeature extends Feature {
+
     final String value;
 
     protected ConfigFeature(String name, String description, int projectId, String value) {
@@ -288,6 +306,7 @@ public class FlagsmithTestHelper {
   }
 
   public static class ProjectEnvironment {
+
     final String apiKey;
     final int projectId;
     final FlagsmithClient client;
@@ -297,11 +316,5 @@ public class FlagsmithTestHelper {
       this.projectId = projectId;
       this.client = client;
     }
-  }
-
-  public static Headers defaultHeaders() {
-    return new Headers(
-        new Header("Content-type", "application/json"),
-        new Header("Authorization", "Token " + IntegrationSuiteTest.TestData.token));
   }
 }
