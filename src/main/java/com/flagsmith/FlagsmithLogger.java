7491bd9030f5c5030cb41596a20241b1fe1c7be2
Pavlo Maks <pavlo@solidstategroup.com>
2021-05-05 10:55:28 +0100

    code refactor

--- src/main/java/com/flagsmith/FlagsmithLogger.java
+++ src/main/java/com/flagsmith/FlagsmithLogger.java
@@ -1,14 +1,14 @@
 package com.flagsmith;
 
+import static java.text.MessageFormat.format;
+
+import java.io.IOException;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.slf4j.Logger;
 
-import java.io.IOException;
-
-import static java.text.MessageFormat.format;
-
 public class FlagsmithLogger {
+
   private Logger logger;
   private FlagsmithLoggerLevel level = FlagsmithLoggerLevel.ERROR;
 
@@ -21,6 +21,13 @@ public class FlagsmithLogger {
     this.logger = logger;
   }
 
+  /**
+   * Based on configuration prints error into log or throws an exception.
+   *
+   * @param request  a Request
+   * @param response a Response
+   * @param doThrow  a boolean whether to throw exception
+   */
   public void httpError(Request request, Response response, boolean doThrow) {
     if (!isLoggingEnabled(FlagsmithLoggerLevel.ERROR) && !doThrow) {
       return;
@@ -30,9 +37,11 @@ public class FlagsmithLogger {
     try {
       body = response.body().string();
     } catch (IOException e) {
-    }
+      // no handling
+    } // ok
 
-    String errorMessage = format("Flagsmith: error when getting flags. Request: {0}, Response: {1} body[{2}]",
+    String errorMessage = format(
+        "Flagsmith: error when getting flags. Request: {0}, Response: {1} body[{2}]",
         request.url(), response, body);
 
     if (doThrow) {
@@ -42,6 +51,13 @@ public class FlagsmithLogger {
     }
   }
 
+  /**
+   * Based on configuration prints error into log or throws an exception.
+   *
+   * @param request   a Request
+   * @param io        an IOException
+   * @param doReThrow a boolean whether to throw exception
+   */
   public void httpError(Request request, IOException io, boolean doReThrow) {
     if (doReThrow) {
       throw new FlagsmithException(io);
@@ -50,12 +66,24 @@ public class FlagsmithLogger {
     }
   }
 
+  /**
+   * Prints error into the log.
+   *
+   * @param var1 a param to print
+   * @param var2 a param to print
+   */
   public void error(String var1, Object... var2) {
     if (isLoggingEnabled(FlagsmithLoggerLevel.ERROR)) {
       logger.error("Flagsmith: " + var1, var2);
     }
   }
 
+  /**
+   * Prints error into the log.
+   *
+   * @param var1 a param to print
+   * @param var2 a param to print
+   */
   public void info(String var1, Object... var2) {
     if (isLoggingEnabled(FlagsmithLoggerLevel.INFO)) {
       logger.info("Flagsmith: " + var1, var2);
