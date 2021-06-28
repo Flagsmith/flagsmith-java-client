dd33a1fc8cd628027ba0756a5774d79d4a4ceaca
Pavlo Maks <pavlo@solidstategroup.com>
2021-06-28 16:21:56 +0100

    code reformatting

--- src/main/java/com/flagsmith/MapperFactory.java
+++ src/main/java/com/flagsmith/MapperFactory.java
@@ -9,14 +9,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class MapperFactory {
 
-    private static volatile ObjectMapper mapper = null;
+  private static volatile ObjectMapper mapper = null;
 
-    public static ObjectMapper getMappper() {
-        if (null == mapper) {
-            mapper = new ObjectMapper();
-            mapper.configure(MapperFeature.USE_ANNOTATIONS, true);
-            mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
-        }
-        return mapper;
+  public static ObjectMapper getMappper() {
+    if (null == mapper) {
+      mapper = new ObjectMapper();
+      mapper.configure(MapperFeature.USE_ANNOTATIONS, true);
+      mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
     }
+    return mapper;
+  }
 }
