8b05a6bb5dbfc88c4724af50efb73325b94499b5
Pavlo Maks <pavlo@solidstategroup.com>
2021-06-29 09:43:49 +0100

    code reformatted

--- src/main/java/com/flagsmith/FlagsAndTraits.java
+++ src/main/java/com/flagsmith/FlagsAndTraits.java
@@ -1,6 +1,5 @@
 package com.flagsmith;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
