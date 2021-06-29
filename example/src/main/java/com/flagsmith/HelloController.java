4cc6028907643a8025cafd62dbee1a4b7b2504cd
Pavlo Maks <pavlo@solidstategroup.com>
2021-06-29 10:10:57 +0100

    code reformatted

--- example/src/main/java/com/flagsmith/HelloController.java
+++ example/src/main/java/com/flagsmith/HelloController.java
@@ -5,14 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-	private static FlagsmithClient flagsmith = FlagsmithClient.newBuilder()
-		.setApiKey("NowEDzKzNJXZVTVanLVdMQ")
-		.build();
 
-	@RequestMapping("/")
-	public String index() {
-		String fontSize = flagsmith.getFeatureFlagValue("font_size");
+  private static FlagsmithClient flagsmith = FlagsmithClient.newBuilder()
+      .setApiKey("NowEDzKzNJXZVTVanLVdMQ")
+      .build();
 
-		return "Greetings from Spring Boot! Font size is " + fontSize;
-	}
+  @RequestMapping("/")
+  public String index() {
+    String fontSize = flagsmith.getFeatureFlagValue("font_size");
+
+    return "Greetings from Spring Boot! Font size is " + fontSize;
+  }
 }
