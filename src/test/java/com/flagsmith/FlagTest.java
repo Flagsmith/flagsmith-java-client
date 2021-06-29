a0b6d26280c8b41b5fb63cedb08d7318a82cb3ee
Pavlo Maks <pavlo@solidstategroup.com>
2021-06-29 14:45:32 +0100

    code reformat

--- src/test/java/com/flagsmith/FlagTest.java
+++ src/test/java/com/flagsmith/FlagTest.java
@@ -1,44 +1,43 @@
 package com.flagsmith;
 
-import org.testng.annotations.Test;
-
-import java.io.IOException;
-
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
+
+import java.io.IOException;
+import org.testng.annotations.Test;
 
 /**
  *
  */
 public class FlagTest {
 
-    private static final String json = "{\n" +
-            "    \"id\": 2,\n" +
-            "    \"feature\": {\n" +
-            "      \"id\": 2,\n" +
-            "      \"name\": \"font_size\",\n" +
-            "      \"created_date\": \"2018-06-04T12:51:18.646762Z\",\n" +
-            "      \"initial_value\": \"10\",\n" +
-            "      \"description\": \"test description\",\n" +
-            "      \"type\": \"CONFIG\",\n" +
-            "      \"project\": 2\n" +
-            "    },\n" +
-            "    \"feature_state_value\": 10,\n" +
-            "    \"enabled\": true,\n" +
-            "    \"environment\": 2,\n" +
-            "    \"identity\": null\n" +
-            "  }";
+  private static final String json = "{\n" +
+      "    \"id\": 2,\n" +
+      "    \"feature\": {\n" +
+      "      \"id\": 2,\n" +
+      "      \"name\": \"font_size\",\n" +
+      "      \"created_date\": \"2018-06-04T12:51:18.646762Z\",\n" +
+      "      \"initial_value\": \"10\",\n" +
+      "      \"description\": \"test description\",\n" +
+      "      \"type\": \"CONFIG\",\n" +
+      "      \"project\": 2\n" +
+      "    },\n" +
+      "    \"feature_state_value\": 10,\n" +
+      "    \"enabled\": true,\n" +
+      "    \"environment\": 2,\n" +
+      "    \"identity\": null\n" +
+      "  }";
 
-    @Test(groups = "unit")
-    public void test_When_Parsed_Then_Success() throws IOException {
-        Flag flag = new Flag();
-        flag.parse(json);
+  @Test(groups = "unit")
+  public void test_When_Parsed_Then_Success() throws IOException {
+    Flag flag = new Flag();
+    flag.parse(json);
 
-        assertNotNull(flag.getStateValue(), "Should have flag value");
-        assertTrue(flag.isEnabled(), "Flag should be enabled");
-        assertNotNull(flag.getFeature(), "Flag should have feature");
-        assertNotNull(flag.getFeature().getName(), "Feature should have type");
-        assertNotNull(flag.getFeature().getType(), "Feature should have name");
-        assertNotNull(flag.getFeature().getDescription(), "Feature should have description");
-    }
+    assertNotNull(flag.getStateValue(), "Should have flag value");
+    assertTrue(flag.isEnabled(), "Flag should be enabled");
+    assertNotNull(flag.getFeature(), "Flag should have feature");
+    assertNotNull(flag.getFeature().getName(), "Feature should have type");
+    assertNotNull(flag.getFeature().getType(), "Feature should have name");
+    assertNotNull(flag.getFeature().getDescription(), "Feature should have description");
+  }
 }
