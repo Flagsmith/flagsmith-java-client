1c79daae69500f2ba8fc77bb536cad15f4446784
Joaquín L. Robles <joaquinlrobles@gmail.com>
2021-12-16 12:19:07 -0300

    Add parameterized constructor (#50)

--- src/main/java/com/flagsmith/FeatureUser.java
+++ src/main/java/com/flagsmith/FeatureUser.java
@@ -1,11 +1,15 @@
 package com.flagsmith;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Representation of the Identity user.
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FeatureUser {
 
   private String identifier;
