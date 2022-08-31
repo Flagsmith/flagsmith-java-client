9076474587d8a8044afe16f73b3e9a612119190b
Matthew Elwell <matthewe@solidstategroup.com>
2022-08-31 15:54:06 +0100

    Fix null pointer error using caching without env cache key

--- src/test/java/com/flagsmith/FlagsmithApiWrapperCachingTest.java
+++ src/test/java/com/flagsmith/FlagsmithApiWrapperCachingTest.java
@@ -7,8 +7,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertThrows;
+import static org.testng.Assert.*;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.flagsmith.config.FlagsmithCacheConfig;
@@ -84,7 +83,7 @@ public class FlagsmithApiWrapperCachingTest {
     // Assert
     verify(flagsmithCacheImpl, times(0)).getCache();
     verify(flagsmithCacheImpl, times(1)).getIfPresent(anyString());
-    verify(flagsmithCacheImpl, times(1)).getEnvFlagsCacheKey();
+    verify(flagsmithCacheImpl, times(2)).getEnvFlagsCacheKey();
 
     assertEquals(flagsAndTraits, actualFeatureFlags);
   }
@@ -113,8 +112,11 @@ public class FlagsmithApiWrapperCachingTest {
   @Test(groups = "unit")
   public void getFeatureFlags_fetchFlagsFromCacheAndNotFromFlagsmith() {
     // Arrange
+    final String cacheKey = "cacheKey";
     final List<FeatureStateModel> flagsAndTraits = new ArrayList<>();
+
     when(flagsmithCacheImpl.getIfPresent(any())).thenReturn(newFlagsList(flagsAndTraits));
+    when(flagsmithCacheImpl.getEnvFlagsCacheKey()).thenReturn(cacheKey);
 
     // Act
     final Flags actualFeatureFlags = flagsmithAPIWrapper.getFeatureFlags(true);
@@ -166,6 +168,22 @@ public class FlagsmithApiWrapperCachingTest {
     assertEquals(newFlagsList(new ArrayList<>()), actualUserFlagsAndTraits);
   }
 
+  @Test(groups = "unit")
+  public void testGetFeatureFlags_ReturnsFeatureFlagsFromApi_IfCacheButNoEnvCacheKey() {
+    // Given
+    when(flagsmithCacheImpl.getEnvFlagsCacheKey()).thenReturn(null);
+
+    final List<FeatureStateModel> featureStateModels = new ArrayList<>();
+    when(requestProcessor.executeAsync(any(), any(), any()))
+            .thenReturn(FlagsmithTestHelper.futurableReturn(featureStateModels));
+
+    // When
+    Flags flags = flagsmithAPIWrapper.getFeatureFlags(false);
+
+    // Then
+    assertEquals(flags, Flags.fromFeatureStateModels(featureStateModels, null));
+  }
+
   public void verifyRequestBody() {
     String identifier = "test-user";
     final ArrayList<TraitModel> traits = new ArrayList<>();
@@ -201,5 +219,4 @@ public class FlagsmithApiWrapperCachingTest {
         flags, null, defaultConfig.getFlagsmithFlagDefaults()
     );
   }
-
 }
