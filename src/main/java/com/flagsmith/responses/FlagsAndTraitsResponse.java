0c6affc1a30e4f825cac81e30a29207be115c307
Matthew Elwell <matthew.elwell@flagsmith.com>
2022-12-19 17:23:55 +0000

    Reorder imports

--- src/main/java/com/flagsmith/responses/FlagsAndTraitsResponse.java
+++ src/main/java/com/flagsmith/responses/FlagsAndTraitsResponse.java
@@ -1,18 +1,17 @@
 package com.flagsmith.responses;
 
-import java.util.List;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.flagsmith.flagengine.features.FeatureStateModel;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class FlagsAndTraitsResponse {
-  List<FeatureStateModel> flags;
-  JsonNode traits;
+    List<FeatureStateModel> flags;
+    JsonNode traits;
 }
