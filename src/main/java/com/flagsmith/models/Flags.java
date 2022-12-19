a39068af8dd95f2e0b8e40600325235eed0daa8f
Andrew Helsby <ajhelsby@hotmail.com>
2022-12-19 19:58:44 +0400

    fix/is-feature-enabled-nullpointer: Add new feature not found error

--- src/main/java/com/flagsmith/models/Flags.java
+++ src/main/java/com/flagsmith/models/Flags.java
@@ -2,6 +2,7 @@ package com.flagsmith.models;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.flagsmith.FlagsmithFlagDefaults;
+import com.flagsmith.exceptions.FeatureNotFoundError;
 import com.flagsmith.exceptions.FlagsmithClientError;
 import com.flagsmith.flagengine.features.FeatureStateModel;
 import com.flagsmith.interfaces.DefaultFlagHandler;
@@ -150,8 +151,12 @@ public class Flags {
    * @param featureName Feature name
    * @return
    */
-  public boolean isFeatureEnabled(String featureName) throws FlagsmithClientError {
-    return getFlag(featureName).getEnabled();
+  public boolean isFeatureEnabled(String featureName)
+      throws FlagsmithClientError {
+    if(!this.flags.containsKey(featureName)) {
+      throw new FeatureNotFoundError("Feature does not exist: " + featureName);
+    }
+    return this.getFlag(featureName).getEnabled();
   }
 
   /**
@@ -175,7 +180,7 @@ public class Flags {
       if (defaultFlagHandler != null) {
         return defaultFlagHandler.evaluateDefaultFlag(featureName);
       }
-      throw new FlagsmithClientError("Feature does not exist: " + featureName);
+      throw new FeatureNotFoundError("Feature does not exist: " + featureName);
     }
 
     BaseFlag flag = flags.get(featureName);
