342ac231b8c111dd5c2c29913849f3c5850701c1
Max Komarychev <maxkomarychev@gmail.com>
2026-04-01 23:44:15 +0200

    Json as a file

--- src/test/java/com/flagsmith/flagengine/unit/mappers/EngineMappersTest.java
+++ src/test/java/com/flagsmith/flagengine/unit/mappers/EngineMappersTest.java
@@ -1,6 +1,5 @@
 package com.flagsmith.flagengine.unit.mappers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.flagsmith.MapperFactory;
 import com.flagsmith.flagengine.EvaluationContext;
 import com.flagsmith.flagengine.FeatureContext;
@@ -13,6 +12,8 @@ import com.flagsmith.models.environments.EnvironmentModel;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -60,59 +61,10 @@ public class EngineMappersTest {
 
   @Test
   public void testMapEnvironmentToContext_preservesMultivariateValueTypes()
-      throws JsonProcessingException {
-    String environmentJson = "{\n"
-        + "  \"api_key\": \"test-key\",\n"
-        + "  \"name\": \"Test\",\n"
-        + "  \"project\": {\n"
-        + "    \"name\": \"Test project\",\n"
-        + "    \"organisation\": {\n"
-        + "      \"feature_analytics\": false,\n"
-        + "      \"name\": \"Test Org\",\n"
-        + "      \"id\": 1,\n"
-        + "      \"persist_trait_data\": true,\n"
-        + "      \"stop_serving_flags\": false\n"
-        + "    },\n"
-        + "    \"id\": 1,\n"
-        + "    \"hide_disabled_flags\": false,\n"
-        + "    \"segments\": []\n"
-        + "  },\n"
-        + "  \"segment_overrides\": [],\n"
-        + "  \"id\": 1,\n"
-        + "  \"feature_states\": [\n"
-        + "    {\n"
-        + "      \"feature_state_value\": true,\n"
-        + "      \"django_id\": 1,\n"
-        + "      \"featurestate_uuid\": \"40eb539d-3713-4720-bbd4-829dbef10d51\",\n"
-        + "      \"feature\": { \"name\": \"mv_feature\", \"type\": \"MULTIVARIATE\", \"id\": 1 },\n"
-        + "      \"enabled\": true,\n"
-        + "      \"multivariate_feature_state_values\": [\n"
-        + "        {\n"
-        + "          \"id\": 1,\n"
-        + "          \"multivariate_feature_option\": { \"value\": false },\n"
-        + "          \"percentage_allocation\": 50.0,\n"
-        + "          \"mv_fs_value_uuid\": \"808cba14-03ca-4835-a7f7-58387f01f87d\"\n"
-        + "        },\n"
-        + "        {\n"
-        + "          \"id\": 2,\n"
-        + "          \"multivariate_feature_option\": { \"value\": 42 },\n"
-        + "          \"percentage_allocation\": 30.0,\n"
-        + "          \"mv_fs_value_uuid\": \"918dbb25-14db-4946-b8a8-69488f02f98e\"\n"
-        + "        },\n"
-        + "        {\n"
-        + "          \"id\": 3,\n"
-        + "          \"multivariate_feature_option\": { \"value\": \"a string\" },\n"
-        + "          \"percentage_allocation\": 20.0,\n"
-        + "          \"mv_fs_value_uuid\": \"a29eca36-25dc-5057-c9b9-7a599f13g09f\"\n"
-        + "        }\n"
-        + "      ]\n"
-        + "    }\n"
-        + "  ],\n"
-        + "  \"identity_overrides\": []\n"
-        + "}";
-
+      throws IOException {
+    InputStream json = getClass().getResourceAsStream("multivariate_environment.json");
     EnvironmentModel env = MapperFactory.getMapper()
-        .readValue(environmentJson, EnvironmentModel.class);
+        .readValue(json, EnvironmentModel.class);
 
     EvaluationContext context = EngineMappers.mapEnvironmentToContext(env);
 
